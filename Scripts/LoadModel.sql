e8dac404f198f315bfcdd3eb8d2e38955eaa623b
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2024-08-11 01:54:25 -0600

    [ADD] LoadModel.sql: Estructura Tablas y Campos

---
+++ Scripts/LoadModel.sql
@@ -0,0 +1,38 @@
+/*
+Flights:
+    - DepartureDate
+    - PassengerID
+    - AirportID
+    - PilotID
+    - StatusID
+
+Passengers:
+    - PassengerID
+    - FirstName
+    - LastName
+    - Gender
+    - Age
+    - Nacionality
+
+Airports:
+    - AirportID
+    - Name
+    - CountryCode
+
+Countries:
+    - CountryCode
+    - Name
+    - ContinentID
+
+Continents:
+    - ContinentID
+    - Name
+
+Pilots:
+    - PilotID
+    - Name
+
+Status:
+    - StatusID
+    - Name
+*/
